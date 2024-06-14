77567a3bb9e24659d6931f8150eb524222af175f
Alexia Christelle <christellealexia53@gmail.com>
2024-06-14 11:39:14 +0200

    Using then function

---
+++ handlingapromise.js
@@ -0,0 +1,13 @@
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+// Write your code below:
+
+function handleSuccess(resolvedValue){
+console.log(resolvedValue);
+};
+function handleFailure(rejectedValue){
+  console.log(rejectedValue);
+};
+ checkInventory(order).then(handleSuccess, handleFailure);
