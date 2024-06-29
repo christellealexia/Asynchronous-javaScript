ff6b247b249755d0ddd9cb088cd8b2734cb17c6e
Alexia Christelle <christellealexia53@gmail.com>
2024-06-30 01:00:17 +0200

    Using callbacks in a function

--- callback.js
+++ callback.js
@@ -1,5 +1,15 @@
-console.log('What day is it today?')
-function myCallback(){
-    console.log('Yess it is Friday')
+// console.log('What day is it today?')
+// function myCallback(){
+//     console.log('Yess it is Friday')
+// }
+// setTimeout(myCallback,2000);
+
+function myFunction(callback){
+    setTimeout(() => {
+        callback()
+    },2000)
 }
-setTimeout(myCallback,2000);
+function today(){
+    console.log('Today is Fridayyyy')
+}
+myFunction(today);
